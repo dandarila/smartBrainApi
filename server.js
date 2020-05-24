ea1948b08cba83aeccdc002ca3cb3b738691f97c
Marian Dandarila <marian_dandarila@yahoo.com>
2020-05-24 20:43:01 +0300

    configure db

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg', 
     connection: {
-        host : 'postgresql-graceful-93123', 
-        user: 'postgres', 
-        password : 'Rocknudrog1@', 
-        database : 'smartb'
+        host : 'process.env.DATABASE_URL', 
+        ssl: true
     }
 });
 
